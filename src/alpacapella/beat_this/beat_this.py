aa0fe250769c14b4c9252ae2971260b44f9535e4
timowendner <timo.wendner@gmx.net>
2026-02-05 07:29:12 +0100

    bug fixes

--- src/alpacapella/beat_this/beat_this.py
+++ src/alpacapella/beat_this/beat_this.py
@@ -2,14 +2,14 @@ from beat_this.inference import File2Beats, Audio2Beats
 import numpy as np
 from .. import annotations
 
-def predict(audio: str | np.ndarray, device: str = "cpu"):
-    if isinstance(audio, np.ndarray):
-        method = Audio2Beats
-    else:
-        method = File2Beats
+def predict(audio: str | np.ndarray, sr=None, device: str = "cpu"):
     checkpoint_path = "final0"
-    model = method(checkpoint_path, device, dbn=False)
-    beats, downbeats = model(audio)
+    if isinstance(audio, np.ndarray):
+        model = Audio2Beats(checkpoint_path, device, dbn=False)
+        beats, downbeats = model(audio, sr)
+    else:
+        model = File2Beats(checkpoint_path, device, dbn=False)
+        beats, downbeats = model(audio)
     return beats, downbeats
 
 
